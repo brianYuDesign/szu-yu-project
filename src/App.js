0b62c232cd91a067a3989c2d24b4fc10a0b37bc1
brian.y <brian.y.tcg@gmail.com>
2022-04-23 11:53:57 +0800

    feat add aos

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import Cover from "./pages/Cover/Cover"
-import Education from "./pages/Education"
-import Info from "./pages/Info"
-import Skill from "./pages/Skill"
-import Work from "./pages/Work"
-import "./App.scss"
-import ThemeToggle from "./components/ThemeToggle"
+import Cover from "./pages/Cover/Cover";
+import Education from "./pages/Education";
+import Info from "./pages/Info";
+import Skill from "./pages/Skill";
+import Work from "./pages/Work";
+import "./App.scss";
+import ThemeToggle from "./components/ThemeToggle";
+import Aos from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 function App() {
+  Aos.init();
   return (
     <div className="mx-auto px-4 py-2 font-semi App">
       <ThemeToggle />
 
       <div className="grid grid-cols-1 gap-3 place-content-stretch items-center ">
-        <div className="">
-          <Cover />
+        <div data-aos="fade-up" data-aos-duration="1000" className="">
+          <Cover x />
         </div>
         <div className="grid grid-cols-1 gap-3   xl:grid-flow-col xl:auto-cols-max">
-          <div className="xl:col-span-2">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            className="xl:col-span-2"
+          >
             <Info />
           </div>
-          <div className="xl:col-span-2">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            className="xl:col-span-2"
+          >
             <Education />
           </div>
-          <div className="xl:row-span-2 ">
+          <div
+            data-aos="fade-left"
+            data-aos-duration="2000"
+            className="xl:row-span-2 "
+          >
             <Skill />
           </div>
         </div>
-        <div className="">
+        <div data-aos="fade-up" data-aos-duration="2000" className="">
           <Work />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
